58a43839c8ca3f91721decae11ed2f08b8cbf242
juan Hernandez <jhernadezgarci@cnm.edu>
2020-11-05 08:10:06 -0700

    crated index.html, persona.html and style.css/started mile stone 2/ try to fix color and background/add columns, change heater color and font.fix my persona/try to fix dropdown.

---
+++ milestone-2/script.js
@@ -0,0 +1,57 @@
+// const post = [{
+//     "id": "1",
+//     "title":"Tacos",
+//     "description": "Comes with onion and fresh cilantro."
+//     },
+//     {
+//         "id": "2",
+//         "title":"Tacos de Canasta",
+//         "description": "Come with your choice of beans,Chorizo with Potato, and Chicharron Aprensado. With a side of curtido(cabbage,onion,carrots and chile)."
+//     },
+//     {
+//         "id": "3",
+//         "title":"Huaraches",
+//         "description":"Come with your choice of meat, beans,lettuce, sour cream, and cheese."
+//     },
+//     {
+//         "id": "4",
+//         "title":"Tortas",
+//         "description":" Comes with your choice of meat, beans, lettuce, tomato, avocado, and toasted Oxaca cheese."
+//     },
+//     {
+//         "id": "5",
+//         "title":"Pambasos",
+//         "description":"Comes with your choice of meat, lettuce, sour cream, and cheese."
+//     },
+//     {
+//         "id": "6",
+//         "title":"Burritos",
+//         "description":"Comes with your choice of meat, beans, lettuce, and cheese."
+//     },
+//     {
+//         "id": "7",
+//         "title":"Quesadilla",
+//         "description":"Comes with your choice of meat, cheese and a side salad with avocado, tomato."
+//     },
+// ]
+//
+// $(document).ready(function(){
+//     $("#menuBtn").onclick(function(){
+//         $("#menu").modal();
+//     });
+// });
+// $(document).ready(function(){
+//     $("#meatBtn").onclick(function(){
+//         $("#meat").modal();
+//     });
+// });
+// $(document).ready(function(){
+//     $("#priceBtn").onclick(function(){
+//         $("#price").modal();
+//     });
+// });
+// $(document).ready(function(){
+//     $("#myBtn").click(function(){
+//         $("#myModal").modal();
+//     });
+// });
